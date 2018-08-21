646364c59762e7cfd4bf53c247d82cbe0361c3d5
Jay Shah <s.jay93@gmail.com>
2018-08-21 11:30:54 -0500

    Initial Commit

---
+++ src/main/java/com/yash/springpemapp/configuration/SpringRootConfig.java
@@ -0,0 +1,13 @@
+package com.yash.springpemapp.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ *  Email sender and other beans are incorporated at the service level so we will declare here.
+ */
+@Configuration
+@ComponentScan(basePackages = {"com.yash"})
+public class SpringRootConfig {
+}
---
+++ src/main/java/com/yash/springpemapp/configuration/SpringWebConfig.java
@@ -0,0 +1,31 @@
+package com.yash.springpemapp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@ComponentScan(basePackages = {"com.yash"})
+@EnableWebMvc
+public class SpringWebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        TODO static resources as css,js
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
